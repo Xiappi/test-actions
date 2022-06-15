22f08407d3d8783b7dc683814cf7224302a4a275
Adam Mattatall <mattatalladam@ymail.com>
2022-06-14 22:42:24 -0300

    s

--- action1/entrypoint.sh
+++ action1/entrypoint.sh
@@ -1,19 +1,26 @@
-#!/bin/bash
-filename='test.txt'
-n=1
+#!/bin/sh -l
 
-# pass current branch, destination branch
-# do git diff
-# process each sql file
+echo "Hello $1"
+time=$(date)
+echo "::set-output name=time::$time" 
 
-while read line; do
 
-if [[ "${line^^}" == *"APPLIED_SCRIPTS"* ]]; then
-    echo "Line No. $n : $line"
-    read line2
-    echo $0
-fi
+# #!/bin/bash
+# filename='test.txt'
+# n=1
 
-n=$((n+1))
-# reading each line
-done < $filename
+# # pass current branch, destination branch
+# # do git diff
+# # process each sql file
+
+# while read line; do
+
+# if [[ "${line^^}" == *"APPLIED_SCRIPTS"* ]]; then
+#     echo "Line No. $n : $line"
+#     read line2
+#     echo $0
+# fi
+
+# n=$((n+1))
+# # reading each line
+# done < $filename
