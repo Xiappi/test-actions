0bdb570b5c3a96159c5b7326fb278fcf3b7eeb59
Adam Mattatall <adam.mattatall@garvin-allen.com>
2022-07-25 15:52:33 -0300

    Working on checking for filename in scripts

--- actions/applied_scripts_action/entrypoint.sh
+++ actions/applied_scripts_action/entrypoint.sh
@@ -1,24 +1,48 @@
 #!/bin/bash
 
+search_dir=./scripts
+reg_expr="^[\d]{6}\w{2}\d{2}.sql$"
+for entry in "$search_dir"/*sql;
+do
+    # check for file
+    if [ -f "$entry" ];
+    then
+        check=0
+
+        # read line
+        while read line;
+        do
+            
+            # check for applied script statement
+            if [[ "${line^^}" == *"APPLIED_SCRIPTS"* ]]; then
+                
+                if [[ $line =~ $reg_expr ]]
+                then
+                    echo $BASH_REMATCH
+                fi
 
 
+                # check next 3 lines for file name
+                # for i in {1..3}
+                # do
+                #     read 
 
-# search_dir=./scripts
-# for entry in "$search_dir"/*sql
-# do
-#   if [ -f "$entry" ];then
-
-#     while read line; do
-
-#     if [[ "${line^^}" == *"APPLIED_SCRIPTS"* ]]; then
-#         echo "Line No. $n : $line"
-#         read line2
-#         echo $0
-#     fi
-
-#     echo "$entry"
-#   fi
-# done
+                # done
+                
+                # echo $line
+                # read line2
+                # echo $line2
+                # echo $entry
+                check=1
+            fi
+        done <$entry
+        
+        if [ $check -eq 0 ]; then
+            
+            echo $entry
+        fi
+    fi
+done
 
 #!/bin/bash
 filename='./scripts/210811DC03.sql'
@@ -28,14 +52,14 @@ n=1
 # do git diff
 # process each sql file
 
-while read line; do
+# while read line; do
 
-if [[ "${line^^}" == *"APPLIED_SCRIPTS"* ]]; then
-    echo "Line No. $n : $line"
-    read line2
-    echo $0
-fi
+# if [[ "${line^^}" == *"APPLIED_SCRIPTS"* ]]; then
+#     echo "Line No. $n : $line"
+#     read line2
+#     echo $0
+# fi
 
-n=$((n+1))
-# reading each line
-done < $filename
+# n=$((n+1))
+# # reading each line
+# done < $filename
