d25a479bb67158add17d8d9ffa3061baf3bde14f
Adam Mattatall <mattatalladam@ymail.com>
2022-07-25 23:34:43 -0300

    Need to loop through changed files

--- .github/workflows/entrypoint.sh
+++ .github/workflows/entrypoint.sh
@@ -1,2 +1,73 @@
 #!/bin/bash
-echo "changed: $1"
+
+# search_dir="../ais8/DatabaseScripts"
+# search_dir="./Scripts"
+changedFiles=$1
+
+file_reg_expr=[0-9]{5,6}[a-zA-Z]{2,3}[0-9]{1,2}
+insert_reg_expr=[0-9]{5,6}[a-zA-Z]{2,3}[0-9]{1,2}[\'\"]
+
+failures=()
+count=0
+
+# TODO: loop through changed files
+
+for entry in  `find $search_dir -type f -name "*.sql" -o`;
+do
+    fileIsValid=0
+    # check for file
+    if [ -f "$entry" ];
+    then
+        check=0
+
+        # capture applied script filename 
+        if [[ $entry =~ $file_reg_expr ]] 
+        then
+            fileName="${BASH_REMATCH[0]}"
+        else
+            # only processe numbered scripts
+            continue
+        fi
+
+        # read line
+        while read line;
+        do
+            
+            # check for applied script statement
+            if [[ "${line^^}" == *"INSERT INTO APPLIED_SCRIPTS"* ]]; 
+            then
+                
+                # grab next line, formatter should put it on the next line
+                read line2
+
+                # capture insert into applied scripts filename, also check the actual insert line to handle cheeky one liners
+                if [[ ${line2^^} =~ ${insert_reg_expr^^} || ${line^^} =~ ${insert_reg_expr^^} ]]
+                then
+
+                    # grab value, and remove trailing quotation
+                   applied_entry="${BASH_REMATCH[0]}" 
+                   applied_entry=${applied_entry::-1}
+                    # compare, we just need one applied scripts entry to match filename so use a flag
+                    if [ ${fileName^^} == ${applied_entry^^} ]
+                    then
+                        fileIsValid=1
+                        break
+                    fi
+                fi
+
+            fi
+        done <$entry
+
+        if [ $fileIsValid -ne 1 ]
+        then
+            failures+=( $entry )
+        fi
+        
+    fi
+done
+
+if [ ${#failures[@]} -ne 0 ]
+then
+    echo ${failures[*]}
+    exit 1
+fi
