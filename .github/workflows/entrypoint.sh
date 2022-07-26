72753763aae94a63061e23c68a274a6f1cb680bf
Adam Mattatall <adam.mattatall@garvin-allen.com>
2022-07-26 10:59:37 -0300

    workin?

--- .github/workflows/entrypoint.sh
+++ .github/workflows/entrypoint.sh
@@ -3,8 +3,8 @@
 # parse input as array, trim first and last character which are [ ]
 IFS=', ' read -r -a array <<< "${1:1:-1}"
 
-file_reg_expr=[0-9]{5,6}[a-zA-Z]{2,3}[0-9]{1,2}
-insert_reg_expr=[0-9]{5,6}[a-zA-Z]{2,3}[0-9]{1,2}[\'\"]
+file_reg_expr=[0-9]{5,6}[a-zA-Z]{2,3}[0-9]{1,3}
+insert_reg_expr=[0-9]{5,6}[a-zA-Z]{2,3}[0-9]{1,3}[\'\"]
 
 failures=()
 count=0
@@ -16,7 +16,7 @@ do
     if [ -f "$entry" ];
     then
         check=0
-        
+
         # capture applied script filename
         if [[ $entry =~ $file_reg_expr ]]
         then
@@ -25,25 +25,27 @@ do
             # only processe numbered scripts
             continue
         fi
-        
+
         # read line
         while read line;
         do
-            
+
             # check for applied script statement
             if [[ "${line^^}" == *"INSERT INTO APPLIED_SCRIPTS"* ]];
             then
-                
+
                 # grab next line, formatter should put it on the next line
                 read line2
-                
+
+
                 # capture insert into applied scripts filename, also check the actual insert line to handle cheeky one liners
                 if [[ ${line2^^} =~ ${insert_reg_expr^^} || ${line^^} =~ ${insert_reg_expr^^} ]]
                 then
-                    
+
                     # grab value, and remove trailing quotation
                     applied_entry="${BASH_REMATCH[0]}"
                     applied_entry=${applied_entry::-1}
+
                     # compare, we just need one applied scripts entry to match filename so use a flag
                     if [ ${fileName^^} == ${applied_entry^^} ]
                     then
@@ -51,15 +53,15 @@ do
                         break
                     fi
                 fi
-                
+
             fi
         done <$entry
-        
+
         if [ $fileIsValid -ne 1 ]
         then
             failures+=( $entry )
         fi
-        
+
     fi
 done
 
